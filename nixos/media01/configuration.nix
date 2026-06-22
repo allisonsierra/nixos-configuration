d95352c6a66f7d5bb78bc39f6f8b2807310cf36c
Allison Sierra <85902135+allisonsierra@users.noreply.github.com>
2026-06-22 14:11:33 -0400

    fix interface

--- nixos/media01/configuration.nix
+++ nixos/media01/configuration.nix
@@ -33,7 +33,7 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   
   networking.useDHCP = false;
-  networking.interfaces.ipv4.addresses = [ {
+  networking.interfaces.ipv4.enp1s0.addresses = [ {
     address = "172.16.1.72";
     prefixLength = 24;
   } ];
