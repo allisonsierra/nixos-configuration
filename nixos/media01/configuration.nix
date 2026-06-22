a6e00bee204fe9e0caa1c19bbf5fdabec30f5dc8
Allison Sierra <85902135+allisonsierra@users.noreply.github.com>
2026-06-22 14:12:01 -0400

    fix

--- nixos/media01/configuration.nix
+++ nixos/media01/configuration.nix
@@ -33,7 +33,7 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   
   networking.useDHCP = false;
-  networking.interfaces.ipv4.enp1s0.addresses = [ {
+  networking.interfaces.enp1s0.ipv4.addresses = [ {
     address = "172.16.1.72";
     prefixLength = 24;
   } ];
