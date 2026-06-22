cb0e0625238d73fcd2ef917d45b8dccbc4568153
Allison Sierra <85902135+allisonsierra@users.noreply.github.com>
2026-06-22 14:04:21 -0400

    fix

--- nixos/media01/configuration.nix
+++ nixos/media01/configuration.nix
@@ -20,7 +20,7 @@
 
   # Bootloader.
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "dev/vda";
+  boot.loader.grub.device = "/dev/vda";
   boot.loader.grub.useOSProber = true;
 
   # Networking
